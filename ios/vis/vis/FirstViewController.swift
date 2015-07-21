1239c77e9047b42312186c0294e6b670021d5c6a
shengoo <shengoo@hotmail.com>
2015-07-21 23:39:05 +0800

    add

---
+++ ios/vis/vis/FirstViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.swift
+//  vis
+//
+//  Created by Qing Sheng on 15/7/20.
+//  Copyright (c) 2015年 Sheng Qing. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
